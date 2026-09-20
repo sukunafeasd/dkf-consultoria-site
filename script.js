2ed0f1e82bb556144575eea32ccd2e81feebee75
Café <moouradieferson5@gmail.com>
2026-09-20 13:45:04 -0300

    Padroniza tema escuro responsivo

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ if ('IntersectionObserver' in window) {
         observer.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.14 });
+  }, { threshold: 0.08, rootMargin: '0px 0px 12% 0px' });
 
   revealItems.forEach((item) => observer.observe(item));
 } else {
